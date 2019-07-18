a9bda42a66e57124c8f1d63eede85a4a7182a4d9
Pavliuk Andrey <52012111+kus-machine@users.noreply.github.com>
2019-07-18 18:19:39 +0300

    Update CalcGUI.py

--- CalcGUI.py
+++ CalcGUI.py
@@ -12,9 +12,11 @@ from kivy.lang import Builder
 from kivy.uix.image import Image
 from kivy.graphics import (Color, Ellipse, Rectangle, Line)
 from kivy.uix.label import Label
-
-Config.set('graphics', 'width', 400)
-Config.set('graphics', 'height', 700)
+screen_width=400
+screen_height=700
+screen_text = '0'
+Config.set('graphics', 'width', screen_width)
+Config.set('graphics', 'height', screen_height)
 Config.set('graphics', 'resizable', 1)
 
 class CalcApp(App):
@@ -25,19 +27,23 @@ class CalcApp(App):
 		kalk_vijet.add_widget(Button(text='1', font_size=30))
 		kalk_vijet.add_widget(Button(text='2', font_size=30))
 		kalk_vijet.add_widget(Button(text='3', font_size=30))
+
 		kalk_vijet.add_widget(Button(text='-', font_size=30))
 		kalk_vijet.add_widget(Button(text='4', font_size=30))
 		kalk_vijet.add_widget(Button(text='5', font_size=30))
 		kalk_vijet.add_widget(Button(text='6', font_size=30))
+
 		kalk_vijet.add_widget(Button(text='*', font_size=30))
 		kalk_vijet.add_widget(Button(text='7', font_size=30))
 		kalk_vijet.add_widget(Button(text='8', font_size=30))
 		kalk_vijet.add_widget(Button(text='9', font_size=30))
+
 		kalk_vijet.add_widget(Button(text='/', font_size=30))
 		kalk_vijet.add_widget(Button(text='.', font_size=30))
 		kalk_vijet.add_widget(Button(text='0', font_size=30))
 		kalk_vijet.add_widget(Button(text='=', font_size=30))
-		output_screen = Label(text='zalupa',size_hint=(1,.20),  pos_hint={'center_x' : 0.5, 'top' :1})
+
+		output_screen = Label(text=screen_text,font_size = 50,size_hint=(1,.20),  pos_hint={'center_x' : 0.5, 'top' :1}, halign='right', text_size = (screen_width-12, screen_height*.20 -12))
 		Calc_mainscreen.add_widget(kalk_vijet)
 		Calc_mainscreen.add_widget(output_screen)
 		return Calc_mainscreen
